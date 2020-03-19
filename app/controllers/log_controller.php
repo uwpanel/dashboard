354a706bd3eb4f9fa6a5eb75ce191791ed6e00eb
Malak Parikh <malak.maskoid@gmail.com>
2020-03-19 13:32:46 +0530

    modifications

---
+++ app/controllers/log_controller.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Default controller if routes are not used
+ *
+ */
+class LogController extends AppController
+{
+
+    public function index()
+    {
+        error_reporting(NULL);
+        $TAB = 'LOG';
+
+        // Main include
+        include(APP_PATH . 'libs/inc/main.php');
+
+        // Data
+        exec(VESTA_CMD . "v-list-user-log $user json", $output, $return_var);
+        check_error($return_var);
+        $data = json_decode(implode('', $output), true);
+        $this->data = array_reverse($data);
+        unset($output);
+
+        // Render page
+        render_page($user, $TAB, 'list_log');
+    }
+    function translate_date($date)
+    {
+        $date = strtotime($date);
+        return strftime("%d &nbsp;", $date) . __(strftime("%b", $date)) . strftime(" &nbsp;%Y", $date);
+    }
+}
---
+++ app/controllers/stats_controller.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Default controller if routes are not used
+ *
+ */
+class StatsController extends AppController
+{
+
+    public function index()
+    {
+
+        error_reporting(NULL);
+        $TAB = 'STATS';
+
+        // Main include
+        include(APP_PATH . 'libs/inc/main.php');
+
+        $user = $_SESSION['user'];
+        // Data
+        if ($user == 'admin') {
+            if (empty($_GET['user'])) {
+                exec(VESTA_CMD . "v-list-users-stats json", $output, $return_var);
+                $data = json_decode(implode('', $output), true);
+                $this->data = array_reverse($data, true);
+                unset($output);
+            } else {
+                $v_user = escapeshellarg($_GET['user']);
+                exec(VESTA_CMD . "v-list-user-stats $v_user json", $output, $return_var);
+                $data = json_decode(implode('', $output), true);
+                $this->data = array_reverse($data, true);
+                unset($output);
+            }
+
+            exec(VESTA_CMD . "v-list-sys-users json", $output, $return_var);
+            $this->users = json_decode(implode('', $output), true);
+            unset($output);
+        } else {
+            exec(VESTA_CMD . "v-list-user-stats $user json", $output, $return_var);
+            $data = json_decode(implode('', $output), true);
+            $this->data = array_reverse($data, true);
+            unset($output);
+        }
+
+        // Render page
+        render_page($user, $TAB, 'list_stats');
+
+        // Back uri
+        $_SESSION['back'] = $_SERVER['REQUEST_URI'];
+    }
+}
