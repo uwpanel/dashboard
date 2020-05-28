be95da23cde672b56c85411fb993c6b5d9fd3a35
Malak Parikh <malakparikh@gmail.com>
2020-05-28 11:18:57 +0530

    Fix - Edit User (Add More Name Server )

--- public/javascript/pages/user/edit.js
+++ public/javascript/pages/user/edit.js
@@ -13,13 +13,14 @@ $(document).ready(function () {
     $('.add-ns-button').click(function () {
         var n = $('input[name^=v_ns]').length;
         if (n < 8) {
-            var t = $($('input[name=v_ns1]').parents('tr')[0]).clone(true, true);
+            var t = $($('input[name=v_ns1]').parents('div')[0]).clone(true, true);
             t.find('input').attr({
                 value: '',
-                name: 'v_ns' + (n + 1)
+                name: 'v_ns' + (n + 1),
+                placeholder: 'Name Server ' + (n + 1),
             });
             t.find('span').show();
-            $('tr.add-ns').before(t);
+            $('div.add-ns').before(t);
         }
         if (n == 7) {
             $('.add-ns').hide();
@@ -27,10 +28,11 @@ $(document).ready(function () {
     });
 
     $('.remove-ns').click(function () {
-        $(this).parents('tr')[0].remove();
+        $(this).parents('div')[0].remove();
         $('input[name^=v_ns]').each(function (i, ns) {
             $(ns).attr({
-                name: 'v_ns' + (i + 1)
+                name: 'v_ns' + (i + 1),
+                placeholder: 'Name Server ' + (i + 1),
             });
             i < 2 ? $(ns).parent().find('span').hide() : $(ns).parent().find('span').show();
         });
